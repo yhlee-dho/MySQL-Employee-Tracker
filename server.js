bc9c9aa4cc3db116c57f8361ff6091e1d84f112a
unknown <yhlee89.dho@gmail.com>
2020-01-21 00:23:47 -0600

    initialize with file structure and cms.sql creation

---
+++ server.js
@@ -0,0 +1,15 @@
+// requirements
+const inquirer = require("inquirer");
+var mysql = require("mysql");
+
+const { sqlPassword } = require("./config.json") 
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: { sqlPassword },
+    database: "cms"
+});
